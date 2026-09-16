c716839ca48379e2bd6eb185006a871ca15ad54a
Shawn Xu <xu107288696@gmail.com>
2026-09-16 14:18:26 -0500

    6x effect 66x elo
    Bench: 785084

--- src/search/move_picker.rs
+++ src/search/move_picker.rs
@@ -222,7 +222,7 @@ impl MovePicker {
             scored.1 = mvv(board, mv) * 8
                 + thread.history.noisy(board, mv) / 8
                 + thread.history.duck(board, mv) / 8
-                + (blocking_set[mv.dest()] & mv.duck().bitboard()).is_nonempty() as i32 * 1000;
+                + (blocking_set[mv.dest()] & mv.duck().bitboard()).is_nonempty() as i32 * 6000;
         }
 
         moves[start..].sort_unstable_by_key(|m| Reverse(m.1));
